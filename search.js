b294295aac58ec996e62255f5a5ab5ac4dc7deee
DianaVS-KS <diana.villalvazo@theksquaregroup.com>
2021-04-19 09:17:49 -0500

    hw1

---
+++ search.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+
+// You need to pass an argument like this: node search C:/Users/Diana Villalvazo/Desktop/Repositorios/KS-NG
+let directoryPath = process.cwd();
+if(process.argv[2] != undefined){
+    const [ , , ...args ] = process.argv;
+    // Fixing if the dir has a space
+    directoryPath = args.join(" ").toString();
+}
+console.log(directoryPath)
+
+try{
+    if(fs.existsSync(directoryPath)){
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+            let route = path.join(directoryPath, file);
+            //Checking if its a directory
+            if(fs.statSync(route).isDirectory()){
+                console.log(file + "/"); 
+            }
+            else{
+                console.log('file: ' + file); 
+            }
+        });
+    })
+}
+else{
+    throw new Error ("Directory not exists") 
+}
+}
+catch(err) {
+    console.log(err.message);
+}
+
